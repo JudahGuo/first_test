53429a2533fecbe1c3b201940778f38fcdf7c4d1
Judah Guo <38379171+JudahGuo@users.noreply.github.com>
2019-01-17 20:45:24 +0800

    上传文件
    第一次使用GitHub上传文件

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3
+int main()
+{
+	int i = -10;//十进制 直接开头
+	int j = 0xa;//十六进制 以0x开头
+	int k = 010;//八进制 以0开头
+	printf("i=%d\n",PI);
+	//printf("Hello World!");
+	system("pause");
+}
